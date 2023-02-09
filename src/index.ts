9d25f13c888f28661eddd7987dd39bd59bb3265e
Avram Walden <aviemet@gmail.com>
2023-02-08 18:17:36 -0800

    Adds imports for all components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useInertiaForm } from './useInertiaForm'
-
 export { default as useInertiaInput } from './useInertiaInput'
+export { default as Form } from './Form'
+export { default as NestedFields } from './NestedFields'
