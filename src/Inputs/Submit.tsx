b064ef3420b1185f5787afb2eee821b706a83144
Avram Walden <aviemet@gmail.com>
2023-06-10 09:10:35 -0700

    feat: 🎸 Adds optional requiredFields prop to Submit button (#8)
    Pass an array of string data paths to Submit to have it be disabled
    until the required fields are not empty

--- src/Inputs/Submit.tsx
+++ src/Inputs/Submit.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import useInertiaForm from '../useInertiaForm'
+import React, { useCallback } from 'react'
+import { useForm } from '../Form'
+import { isEmpty } from 'lodash'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	component?: string | React.ComponentType
+	requiredFields?: string[]
 }
 
 const Submit = React.forwardRef<HTMLButtonElement, ButtonProps>((
-	{ children, type = 'submit', disabled, component = 'button', ...props },
+	{ children, type = 'submit', disabled = false, component:Component = 'button', requiredFields, ...props },
 	ref,
 ) => {
-	const { processing } = useInertiaForm()
+	const { data, getData, processing } = useForm()
 
-	const Element = component
+	const hasEmptyRequiredFields = useCallback(() => {
+		if(!requiredFields || requiredFields.length === 0) return false
+
+		return requiredFields.some((field) => isEmpty(getData(field)))
+	}, [data])
 
 	return (
-		<Element { ...{ children, type, disabled: disabled || processing, ref, ...props } } />
+		<Component { ...{
+			children,
+			type,
+			disabled: disabled || processing || (requiredFields && hasEmptyRequiredFields()),
+			ref,
+			...props,
+		} } />
 	)
 })
 
-export default Submit
+export default React.memo(Submit)
