5133aeec5d73f47d2d95078ca8fcf2e968c25d2e
Avram Walden <aviemet@gmail.com>
2024-11-27 10:05:07 -0800

    test: adds tests for non-nested data

--- tests/useInertiaForm.test.ts
+++ tests/useInertiaForm.test.ts
@@ -10,7 +10,7 @@ type InitialData = {
 	person: {
 		first_name?: string
 		last_name?: string
-		middle_name?: string|undefined
+		middle_name?: string | undefined
 	}
 	contact: {
 		phones: { number?: string }[]
@@ -35,108 +35,355 @@ const initialData: InitialData = {
 	},
 }
 
-describe('useInertiaForm', () => {
-	const { result } = renderHook(() => useInertiaForm(initialData))
+const flatData: Record<string, string | undefined> = {
+	first_name: 'first',
+	middle_name: undefined,
+	last_name: 'last',
+}
 
-	it('data value should be equal to initialData, with undefined values converted to empty strings', () => {
-		const expectedValue = structuredClone(initialData)
-		expectedValue.person.middle_name = ''
-		expect(result.current.data).toStrictEqual(expectedValue)
+const singleValue: Record<string, string | undefined> = {
+	first_name: undefined,
+}
+
+describe('useInertiaForm', () => {
+	describe('with nested data', () => {
+		const { result } = renderHook(() => useInertiaForm(initialData))
+
+		it('data value should be equal to initialData, with undefined values converted to empty strings', () => {
+			const expectedValue = structuredClone(initialData)
+			expectedValue.person.middle_name = ''
+			expect(result.current.data).toStrictEqual(expectedValue)
+		})
+	})
+
+	describe('with flat data', () => {
+		const { result } = renderHook(() => useInertiaForm(flatData))
+
+		it('data value should be equal to flatData, with undefined values converted to empty strings', () => {
+			const expectedValue = structuredClone(flatData)
+			expectedValue.middle_name = ''
+			expect(result.current.data).toStrictEqual(expectedValue)
+		})
+	})
+
+	describe('with only one key', () => {
+		const { result } = renderHook(() => useInertiaForm(singleValue))
+
+		it('data value should be equal to singleValue, with undefined values converted to empty strings', () => {
+			const expectedValue = structuredClone(singleValue)
+			expectedValue.first_name = ''
+			expect(result.current.data).toStrictEqual(expectedValue)
+		})
 	})
 })
 
 describe('setData', () => {
-	it('should update nested state', () => {
+	describe('with nested data', () => {
+		it('should update nested state', () => {
 
-		const { result } = renderHook(() => useInertiaForm(initialData))
-		act(() => {
-			result.current.setData('user.username', 'changed')
-			result.current.setData('contact.phones[0].number', '234567')
-			result.current.setData('contact.phones[3].number', 'new number')
+			const { result } = renderHook(() => useInertiaForm(initialData))
+			act(() => {
+				result.current.setData('user.username', 'changed')
+			})
+
+			expect(result.current.data?.user.username).toStrictEqual('changed')
 		})
 
-		expect(result.current.data?.user.username).toStrictEqual('changed')
-		expect(result.current.data?.contact.phones[0].number).toStrictEqual('234567')
-		expect(result.current.data?.contact.phones[3].number).toStrictEqual('new number')
+		it('should update nested array values', () => {
+
+			const { result } = renderHook(() => useInertiaForm(initialData))
+			act(() => {
+				result.current.setData('contact.phones[0].number', '234567')
+				result.current.setData('contact.phones[3].number', 'new number')
+			})
+
+			expect(result.current.data?.contact.phones[0].number).toStrictEqual('234567')
+			expect(result.current.data?.contact.phones[3].number).toStrictEqual('new number')
+		})
+	})
+
+	describe('with flat data', () => {
+		it('should update state', () => {
+
+			const { result } = renderHook(() => useInertiaForm(flatData))
+			act(() => {
+				result.current.setData('first_name', 'changed')
+			})
+
+			expect(result.current.data?.first_name).toStrictEqual('changed')
+		})
+	})
+
+	describe('with only one key', () => {
+		it('should update state', () => {
+
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+			act(() => {
+				result.current.setData('first_name', 'changed')
+			})
+
+			expect(result.current.data?.first_name).toStrictEqual('changed')
+		})
 	})
 })
 
 describe('getData', () => {
-	it('should get nested state', () => {
-		const { result } = renderHook(() => useInertiaForm(initialData))
+	describe('with nested data', () => {
+		it('should get initial nested state', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
 
-		act(() => {
-			expect(result.current.getData('user.username')).toStrictEqual(initialData.user.username)
-			expect(result.current.getData('contact.phones[0].number')).toStrictEqual(initialData.contact.phones[0].number)
+			act(() => {
+				expect(result.current.getData('user.username')).toStrictEqual(initialData.user.username)
+				expect(result.current.getData('contact.phones[0].number')).toStrictEqual(initialData.contact.phones[0].number)
+			})
+		})
+
+		it('should get updated nested state', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
+
+			act(() => {
+				result.current.setData('user.username', 'something')
+				result.current.setData('contact.phones[0].number', '123-456-7890')
+			})
+
+			act(() => {
+				expect(result.current.getData('user.username')).toStrictEqual('something')
+				expect(result.current.getData('contact.phones[0].number')).toStrictEqual('123-456-7890')
+			})
+		})
+	})
+
+	describe('with flat data', () => {
+		it('should get initial state', () => {
+			const { result } = renderHook(() => useInertiaForm(flatData))
+
+			act(() => {
+				expect(result.current.getData('first_name')).toStrictEqual(flatData.first_name)
+				expect(result.current.getData('middle_name')).toStrictEqual('')
+			})
+		})
+
+		it('should get updated state', () => {
+			const { result } = renderHook(() => useInertiaForm(flatData))
+
+			act(() => result.current.setData('middle_name', 'something'))
+
+			act(() => {
+				expect(result.current.getData('middle_name')).toStrictEqual('something')
+			})
+		})
+	})
+
+	describe('with only one key', () => {
+		it('should get nested state', () => {
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+
+			act(() => {
+				expect(result.current.getData('first_name')).toStrictEqual('')
+			})
+		})
+	})
+
+	describe('with only one key', () => {
+		it('should get updated state', () => {
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+
+			act(() => result.current.setData('first_name', 'something'))
+
+			act(() => {
+				expect(result.current.getData('first_name')).toStrictEqual('something')
+			})
 		})
 	})
 })
 
 describe('unsetData', () => {
-	it('should remove nested state', () => {
-		const { result } = renderHook(() => useInertiaForm(initialData))
+	describe('with nested data', () => {
+		it('should remove nested state', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
 
-		act(() => result.current.unsetData('user.username'))
+			act(() => result.current.unsetData('user.username'))
 
-		act(() => expect(result.current.data?.user).toMatchObject({}))
+			act(() => expect(result.current.data?.user).toMatchObject({}))
+		})
+
+
+		it('should reorder arrays when removing an element', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
+
+			act(() => result.current.unsetData('contact.phones[1]'))
+
+			act(() => {
+				expect(result.current.data?.contact.phones[0].number).toStrictEqual('1234567890')
+				expect(result.current.data?.contact.phones[1].number).toStrictEqual('3234567890')
+			})
+		})
 	})
 
+	describe('with flat data', () => {
+		it('should remove a value', () => {
+			const { result } = renderHook(() => useInertiaForm(flatData))
 
-	it('should reorder arrays when removing an element', () => {
-		const { result } = renderHook(() => useInertiaForm(initialData))
+			act(() => result.current.unsetData('first_name'))
 
-		act(() => result.current.unsetData('contact.phones[1]'))
+			act(() => {
+				expect(result.current.data).not.toHaveProperty('first_name')
+				// Others are untouched
+				expect(result.current.data?.last_name).toStrictEqual(flatData.last_name)
+			})
+		})
+	})
 
-		act(() => {
-			expect(result.current.data?.contact.phones[0].number).toStrictEqual('1234567890')
-			expect(result.current.data?.contact.phones[1].number).toStrictEqual('3234567890')
+	describe('with only one key', () => {
+		it('should remove a value', () => {
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+
+			act(() => result.current.unsetData('first_name'))
+
+			act(() => {
+				expect(result.current.data).not.toHaveProperty('first_name')
+				expect(result.current.data).toEqual({})
+			})
 		})
 	})
 })
 
 // Tests not strictly necessary since we don't override setError
 describe('setError', () => {
-	it('should set errors by key', () => {
-		const { result } = renderHook(() => useInertiaForm(initialData))
+	describe('with nested data', () => {
+		it('should set errors by key', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
 
-		const key = 'person.middle_name'
-		const error = 'Value must not be empty'
+			const key = 'person.middle_name'
+			const error = 'Value must not be empty'
 
-		act(() => result.current.setError(key, error))
+			act(() => result.current.setError(key, error))
 
-		act(() => {
-			expect(result.current.errors).toStrictEqual({ [key]: error })
-			expect(result.current.hasErrors).toBe(true)
+			act(() => {
+				expect(result.current.errors).toStrictEqual({ [key]: error })
+				expect(result.current.hasErrors).toBe(true)
+			})
+		})
+
+		it('should set errors by object', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
+
+			const errors = {
+				'person.middle_name': 'Value must not be empty',
+				'contact.phones[1].number': 'Value is no good!',
+			}
+
+			act(() => result.current.setError(errors))
+
+			act(() => {
+				expect(result.current.errors).toStrictEqual(errors)
+				expect(result.current.hasErrors).toBe(true)
+			})
 		})
 	})
 
-	it('should set errors by object', () => {
-		const { result } = renderHook(() => useInertiaForm(initialData))
+	describe('with flat data', () => {
+		it('should set errors by key', () => {
+			const { result } = renderHook(() => useInertiaForm(flatData))
 
-		const errors = {
-			'person.middle_name': 'Value must not be empty',
-			'contact.phones[1].number': 'Value is no good!',
-		}
+			const key = 'middle_name'
+			const error = 'Value must not be empty'
 
-		act(() => result.current.setError(errors))
+			act(() => result.current.setError(key, error))
 
-		act(() => {
-			expect(result.current.errors).toStrictEqual(errors)
-			expect(result.current.hasErrors).toBe(true)
+			act(() => {
+				expect(result.current.errors).toStrictEqual({ [key]: error })
+				expect(result.current.hasErrors).toBe(true)
+			})
+		})
+
+		it('should set errors by object', () => {
+			const { result } = renderHook(() => useInertiaForm(flatData))
+
+			const errors = {
+				'first_name': 'Value must not be empty',
+				'middle_name': 'Value is no good!',
+			}
+
+			act(() => result.current.setError(errors))
+
+			act(() => {
+				expect(result.current.errors).toStrictEqual(errors)
+				expect(result.current.hasErrors).toBe(true)
+			})
+		})
+	})
+
+	describe('with only one key', () => {
+		it('should set errors by key', () => {
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+
+			const key = 'first_name'
+			const error = 'Value must not be empty'
+
+			act(() => result.current.setError(key, error))
+
+			act(() => {
+				expect(result.current.errors).toStrictEqual({ [key]: error })
+				expect(result.current.hasErrors).toBe(true)
+			})
+		})
+
+		it('should set errors by object', () => {
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+
+			const errors = {
+				'first_name': 'Value must not be empty',
+			}
+
+			act(() => result.current.setError(errors))
+
+			act(() => {
+				expect(result.current.errors).toStrictEqual(errors)
+				expect(result.current.hasErrors).toBe(true)
+			})
 		})
 	})
 })
 
 describe('getError', () => {
-	it('should return a single error by key', () => {
-		const { result } = renderHook(() => useInertiaForm(initialData))
+	describe('with nested data', () => {
+		it('should return a single error by key', () => {
+			const { result } = renderHook(() => useInertiaForm(initialData))
 
-		const key = 'person.middle_name'
-		const error = 'Value must not be empty'
+			const key = 'person.middle_name'
+			const error = 'Value must not be empty'
 
-		act(() => result.current.setError(key, error))
+			act(() => result.current.setError(key, error))
 
-		act(() => expect(result.current.getError(key)).toBe(error))
+			act(() => expect(result.current.getError(key)).toBe(error))
+		})
+	})
+
+	describe('with flat data', () => {
+		it('should return a single error by key', () => {
+			const { result } = renderHook(() => useInertiaForm(flatData))
+
+			const key = 'middle_name'
+			const error = 'Value must not be empty'
+
+			act(() => result.current.setError(key, error))
+
+			act(() => expect(result.current.getError(key)).toBe(error))
+		})
+	})
+
+	describe('with single value', () => {
+		it('should return a single error by key', () => {
+			const { result } = renderHook(() => useInertiaForm(singleValue))
+
+			const key = 'first_name'
+			const error = 'Value must not be empty'
+
+			act(() => result.current.setError(key, error))
+
+			act(() => expect(result.current.getError(key)).toBe(error))
+		})
 	})
 })
 
@@ -270,8 +517,7 @@ describe('onChange', () => {
 })
 
 describe('submit', () => {
-	it('should submit the correct data to the server', () => {
-
+	it('should submit transformed data to the server', () => {
 		const testData = {
 			user: {
 				username: 'some name',
@@ -288,7 +534,52 @@ describe('submit', () => {
 		act(() => {
 			result.current.transform(data => ({ ...data, transformed: 'value' }))
 			result.current.submit('post', '/form')
-			expect(mockRequest).toBeCalled()
+			expect(mockRequest).toHaveBeenCalled()
+		})
+	})
+
+	describe('onError', () => {
+		describe('with nested data', () => {
+			it('should set errors from server response', async () => {
+				const testData = {
+					user: {
+						username: '',
+					},
+				}
+
+				axios.mockResolvedValueOnce({
+					response: {
+						errors: {
+							username: ['must exist'],
+						},
+					},
+				});
+
+				const { result } = renderHook(() => useInertiaForm(testData))
+
+				const mockRequest = jest.spyOn(router, 'visit').mockImplementation(() => {
+					return Promise.resolve({
+						errors: {
+							username: ["must exist"],
+						},
+					})
+				})
+
+				await act(async () => {
+					await result.current.submit('post', '/form')
+				})
+
+				act(() => {
+					console.log({ result: result.current })
+					expect(result.current.errors).toEqual({
+						username: ["must exist"],
+					});
+					expect(mockRequest).toHaveBeenCalledWith('/form', expect.objectContaining({ data: testData }));
+
+					// expect(mockRequest).toHaveBeenCalled()
+				})
+			})
 		})
 	})
 })
+
